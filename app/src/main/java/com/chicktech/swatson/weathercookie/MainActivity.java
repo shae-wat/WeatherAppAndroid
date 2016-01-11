d051f1ada07096c4c02dd0bc4aebf981d7bca5d0
Shaelyn <shaelynjoy@gmail.com>
2016-01-10 22:06:56 -0800

    Async call to weather data- Forecast.io

--- app/src/main/java/com/chicktech/swatson/weathercookie/MainActivity.java
+++ app/src/main/java/com/chicktech/swatson/weathercookie/MainActivity.java
@@ -5,11 +5,22 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +37,34 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        String apiKey = "85709eec804fd0f1065b210fa3dc2548";
+        double latitude = 37.8267;
+        double longitude = -122.423;
+        String forecastURL = "https://api.forecast.io/forecast/" + apiKey + "/" + latitude + "," + longitude;
+
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(forecastURL).build();
+
+        Call call = client.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Response response) throws IOException {
+                try {
+                    if (response.isSuccessful()){
+                        Log.e(TAG, response.body().string());
+                    }
+                } catch (IOException e) {
+                    Log.e(TAG, "Exception caught: ", e);
+                }
+            }
+        });
+
     }
 
     @Override
